995e50aff1b66d8976043257d4edf56ff56d72f2
Precious Betine <preciousbetine@gmail.com>
2023-02-21 22:19:17 +0100

    Create array of todo tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import './style.scss';
 
-
+const todoList = [
+  {
+    description: 'Task 1',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Task 2',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Task 3',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Task 4',
+    completed: false,
+    index: 3,
+  },
+];
